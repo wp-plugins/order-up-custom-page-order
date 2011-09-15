9e5787813b869e5fa97ca85b60ba4a53943aef43
dgourley <dgourley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-15 15:32:10 +0000

    updating to version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/order-up-custom-page-order/trunk@438588 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custompageorder.php
+++ custompageorder.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Custom Page Order
-Plugin URI: http://www.leopard-inc.com
+Plugin URI: http://drewgourley.com
 Description: Allows for the ordering of pages through a simple drag-and-drop interface.
-Version: 1.0
+Version: 2.0
 Author: Drew Gourley
 */
 function custompageorder_menu() {    
@@ -24,139 +24,152 @@ function custompageorder_js_libs() {
 	}
 }
 add_action('admin_menu', 'custompageorder_menu');
-add_action('admin_menu', 'custompageorder_css');
+add_action('admin_print_styles', 'custompageorder_css');
 add_action('admin_print_scripts', 'custompageorder_js_libs');
 
 function custompageorder() {
 	global $wpdb;
-	$parentID = 0;
-	if (isset($_POST['btnSubPages'])) { 
-		$parentID = $_POST['pages'];
+	$parent_ID = 0;
+	if (isset($_POST['go-sub-pages'])) { 
+		$parent_ID = $_POST['sub-pages'];
 	}
-	elseif (isset($_POST['hdnParentID'])) { 
-		$parentID = $_POST['hdnParentID'];
+	elseif (isset($_POST['hidden-parent-id'])) { 
+		$parent_ID = $_POST['hidden-parent-id'];
 	}
-	if (isset($_POST['btnReturnParent'])) { 
-		$parentsParent = $wpdb->get_row("SELECT post_parent FROM $wpdb->posts WHERE ID = " . $_POST['hdnParentID'], ARRAY_N);
-		$parentID = $parentsParent[0];
+	if (isset($_POST['return-sub-pages'])) { 
+		$parent_post = get_post($_POST['hidden-parent-id']);
+		$parent_ID = $parent_post->post_parent;
 	}
-	$success = "";
-	if (isset($_POST['btnOrderPages'])) { 
-		$success = custompageorder_updateOrder();
+	$message = "";
+	if (isset($_POST['order-submit'])) { 
+		$message = custompageorder_update_order();
 	}
-	$subPageStr = custompageorder_getSubPages($parentID);
 ?>
 <div class='wrap'>
-	<form name="frmCustomPageOrder" method="post" action="">
+	<form name="custom-order-form" method="post" action="">
 		<?php screen_icon('custompageorder'); ?>
-		<h2><?php _e('Order Pages', 'custompageorder') ?></h2>
-		<?php echo $success; ?>
-		<?php if( $subPageStr ) { ?>
-		<h3><?php _e('Order Subpages', 'custompageorder') ?></h3>
-		<p><?php _e('Choose a page from the drop down to order its subpages.', 'custompageorder') ?></p>
-		<select id="pages" name="pages">
-			<?php echo $subPageStr; ?>
-		</select>
-		<input type="submit" name="btnSubPages" class="button" id="btnSubPages" value="<?php _e('Order Subpages', 'custompageorder') ?>" />
-		<?php } ?>
-		<?php $results = custompageorder_pageQuery($parentID); 
-		if ( $results ) { ?>
-		<p><?php _e('Order the pages by dragging and dropping them into the desired order.', 'custompageorder') ?></p>
-		<div class="metabox-holder">
-			<div class="postbox-container" style="width:80%">
-				<div class="stuffbox">
-					<h3><?php _e('Pages', 'custompageorder') ?></h3>
-					<div id="minor-publishing">
-						<ul id="customPageOrderList">
-							<?php foreach($results as $row) { echo '<li id="id_'.$row->ID.'" class="lineitem">'.__($row->post_title).'</li>'; } ?>
-						</ul>
-					</div>
-					<div id="major-publishing-actions">
-						<?php echo custompageorder_getParentLink($parentID); ?>
-						<div id="publishing-action">
-							<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" id="custom-loading" style="visibility:hidden;" alt="" />
-							<input type="submit" name="btnOrderPages" id="btnOrderPages" class="button-primary" value="<?php _e('Update Page Order', 'custompageorder') ?>" onclick="javascript:orderPages(); return true;" />
-						</div>
-						<div class="clear"></div>
-					</div>
-					<input type="hidden" id="hdnCustomPageOrder" name="hdnCustomPageOrder" />
-					<input type="hidden" id="hdnParentID" name="hdnParentID" value="<?php echo $parentID; ?>" />
+		<h2><?php _e('Order Pages', 'custompageorder'); ?></h2>
+		<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+		<?php $args = array( 
+			'post_parent' => $parent_ID, 
+			'post_type' => 'page',
+			'posts_per_page' => get_option('posts_per_page'),
+			'paged' => max( 1, $_GET['paged'] )
+			);
+		$query = new WP_Query( $args );
+		if ( $query->have_posts() ) { ?>
+		<div id="poststuff" class="metabox-holder">
+			<div class="widget order-widget">
+				<h3 class="widget-top"><?php _e('Pages', 'custompageorder') ?> | <small><?php _e('Order the pages by dragging and dropping them into the desired order.', 'custompageorder') ?></small></h3>
+				<div class="misc-pub-section">
+					<ul id="custom-order-list">
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+						<li id="id_<?php the_ID(); ?>" class="lineitem"><?php the_title(); ?></li>
+						<?php endwhile; ?>
+					</ul>
 				</div>
+				<?php $big = 32768;
+				$args = array(
+					'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+					'format' => '?paged=%#%',
+					'prev_next' => false,
+					'current' => max( 1, $_GET['paged'] ),
+					'total' => $query->max_num_pages
+					); 
+				$pagination = paginate_links($args); 
+				if ( !empty($pagination) ) { ?>
+				<div class="misc-pub-section">
+					<div class="tablenav" style="margin:0">
+						<div class="tablenav-pages">
+							<span class="pagination-links"><?php echo $pagination; ?></span>
+						</div>
+					</div>
+				</div>
+				<?php } ?>
+				<div class="misc-pub-section misc-pub-section-last">
+					<?php if ($parent_ID != 0) { ?>
+						<input type="submit" class="button" style="float:left" id="return-sub-pages" name="return-sub-pages" value="<?php _e('Return to Parent Page', 'custompageorder'); ?>" />
+					<?php } ?>
+					<div id="publishing-action">
+						<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" id="custom-loading" style="display:none" alt="" />
+						<input type="submit" name="order-submit" id="order-submit" class="button-primary" value="<?php _e('Update Page Order', 'custompageorder') ?>" />
+					</div>
+					<div class="clear"></div>
+					</div>
+				<input type="hidden" id="hidden-custom-order" name="hidden-custom-order" />
+				<input type="hidden" id="hidden-parent-id" name="hidden-parent-id" value="<?php echo $parent_ID; ?>" />
 			</div>
+			<?php $options = custompageorder_sub_query($query); if( !empty($options) ) { ?>
+			<div class="widget order-widget">
+				<h3 class="widget-top"><?php _e('Subpages', 'custompageorder'); ?> | <small><?php _e('Choose a page from the drop down to order its subpages.', 'custompageorder'); ?></small></h3>
+				<div class="misc-pub-section misc-pub-section-last">
+					<select id="sub-pages" name="sub-pages">
+						<?php echo $options; ?>
+					</select>
+					<input type="submit" name="go-sub-pages" class="button" id="go-sub-pages" value="<?php _e('Order Subpages', 'custompageorder') ?>" />
+				</div>
+			</div>		
+			<?php } ?>
 		</div>
 		<?php } else { ?>
 		<p><?php _e('No pages found', 'customtaxorder'); ?></p>
 		<?php } ?>
 	</form>
 </div>
-<?php if ( $results ) { ?>
+<?php if ( $query->have_posts() ) { ?>
 <script type="text/javascript">
 // <![CDATA[
-	function custompageorderaddloadevent(){
-		jQuery("#customPageOrderList").sortable({ 
+	jQuery(document).ready(function($) {
+		$("#custom-loading").hide();
+		$("#order-submit").click(function() {
+			orderSubmit();
+		});
+	});
+	function custompageorderAddLoadEvent(){
+		jQuery("#custom-order-list").sortable({ 
 			placeholder: "sortable-placeholder", 
 			revert: false,
 			tolerance: "pointer" 
 		});
 	};
-	addLoadEvent(custompageorderaddloadevent);
-	function orderPages() {
-		jQuery("#custom-loading").css( "visibility", "visible" );
-		jQuery("#hdnCustomPageOrder").val(jQuery("#customPageOrderList").sortable("toArray"));
+	addLoadEvent(custompageorderAddLoadEvent);
+	function orderSubmit() {
+		var newOrder = jQuery("#custom-order-list").sortable("toArray");
+		jQuery("#custom-loading").show();
+		jQuery("#hidden-custom-order").val(newOrder);
+		return true;
 	}
 // ]]>
 </script>
 <?php }
 }
 
-function custompageorder_updateOrder() {
-	if (isset($_POST['hdnCustomPageOrder']) && $_POST['hdnCustomPageOrder'] != "") { 
+function custompageorder_update_order() {
+	if (isset($_POST['hidden-custom-order']) && $_POST['hidden-custom-order'] != "") { 
 		global $wpdb;
-		$hdnCustomPageOrder = $_POST['hdnCustomPageOrder'];
-		$IDs = explode(",", $hdnCustomPageOrder);
+		$offset = ( max( 1, $_GET['paged'] ) - 1 ) * get_option('posts_per_page');
+		$new_order = $_POST['hidden-custom-order'];
+		$IDs = explode(",", $new_order);
 		$result = count($IDs);
 		for($i = 0; $i < $result; $i++)	{
 			$str = str_replace("id_", "", $IDs[$i]);
-			$wpdb->query("UPDATE $wpdb->posts SET menu_order = '$i' WHERE id ='$str'");
+			$order = $i + $offset;
+			$update = array('ID' => $str, 'menu_order' => $order);
+			wp_update_post( $update );
 		}
 		return '<div id="message" class="updated fade"><p>'. __('Page order updated successfully.', 'custompageorder').'</p></div>';
 	} else {
 		return '<div id="message" class="error fade"><p>'. __('An error occured, order has not been saved.', 'custompageorder').'</p></div>';
 	}
 }
-
-function custompageorder_getSubPages($parentID) {
-	global $wpdb;
-	$query_result = custompageorder_pageQuery($parentID);
-	foreach($query_result as $row) {
-		$post_count=$wpdb->get_row("SELECT count(*) as postsCount FROM $wpdb->posts WHERE post_parent = $row->ID and post_type = 'page' AND post_status != 'trash' AND post_status != 'auto-draft' ", ARRAY_N);
-		if($post_count[0] > 0) {
-	    	$subPageStr .= '<option value="'.$row->ID.'">'.__($row->post_title).'</option>';
-		}
-	}
-	if ( $subPageStr ) {
-		return $subPageStr;
-	} else {
-		return false;
-	}
-}
-
-function custompageorder_pageQuery($parentID) {
-	global $wpdb;
-	$query_result = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = $parentID and post_type = 'page' AND post_status != 'trash' AND post_status != 'auto-draft' ORDER BY menu_order ASC");
-	if ( empty($query_result) ) {
-		return false;
-	} else {
-		return $query_result;
-	}
-}
-
-function custompageorder_getParentLink($parentID) {
-	if($parentID != 0) {
-		return '<input type="submit" class="button" style="float:left" id="btnReturnParent" name="btnReturnParent" value="'. __('Return to Parent Page', 'custompageorder') .'" />';
-	} else {
-		return "";
-	}
+function custompageorder_sub_query( $query ) {
+	$options = '';
+	while ( $query->have_posts() ) : $query->the_post(); $page_ID = get_the_ID(); $args = array( 'post_parent' => $page_ID, 'post_type' => 'page' ); $subpages = new WP_Query( $args );
+		if ( $subpages->have_posts() ) { 
+			while ( $subpages->have_posts() ) : $subpages->the_post(); $options .= '<option value="' . $page_ID . '">' . get_the_title($page_ID) . '</option>'; endwhile; 
+		} 
+	endwhile;
+	return $options;
 }
 
 function custompageorder_order_pages($orderby) {
@@ -164,7 +177,6 @@ function custompageorder_order_pages($orderby) {
 	$orderby = "$wpdb->posts.menu_order ASC";
 	return $orderby;
 }
-
 function custompageorder_sort() {
 	if (!is_admin() || is_admin() && !isset($_GET['orderby'])) {
 		add_filter('posts_orderby', 'custompageorder_order_pages');
